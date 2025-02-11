561d6680ac29b4e272eb5a1f4f7a60495246388c
Roman Zalewski <r.a.zalewski@gmail.com>
2025-02-10 19:40:16 -0700

    final, data-output

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 import requests as r
-from sys import argv
 from datetime import date
-
+import json
+from sys import argv
 headers = {
-'content-type' : 'application/json; charset=utf-8',
-'server' : 'Kestrel',
-'lang' : 'en',
-'srctype' : 'default',
-'rid' : 'bd54fee9-52f5-4aee-b25b-7221444de080',
-'gid' : '204',
-'x-edgeconnect-midmile-rtt' : '31',
-'x-edgeconnect-origin-mex-latency' : '210',
-'cache-control' : 'max-age=0, no-cache, no-store',
-'pragma' : 'no-cache',
+'user-agent' : 'Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405'
 }
 
+def download_data(ticker: str) -> dict:
+    ticker = ticker.upper()
+    today = date.today()
+    start = str(today.replace(year=today.year - 5))
+    url = f"https://api.nasdaq.com/api/quote/{ticker}/historical?assetclass=stocks&fromdate={start}&limit=9999"
+    response = r.get(url, headers = headers)
+    data = response.json()
+    return data
 
-def test():
-    req = r.get('https://api.nasdaq.com/api/quote/aapl/historical?assetclass=stocks&fromdate=2020-02-02&limit=9999', headers = headers, timeout = 10)
 
-    print(req.json())
-    print(date.today())
+def process(data:dict) -> dict:
+    rows = data['data']['tradesTable']['rows']
+    closing = []
+    for row in rows:
+        close=float(row['close'][1:])
+        closing.append(close)
+    final = {}
+    final['max'] = max(closing)
+    final['min'] = min(closing)
+    final['avg'] = sum(closing) / len(closing)
+    sorted_price = sorted(closing)
+    middle_price = len(closing) // 2
+    if len(sorted_price) % 2 != 0:
+        final['median'] = sorted_price[len(closing) // 2]
+    else:
+        final['median'] = (sorted_price[len(closing) // 2-1] + sorted_price[len(closing) // 2]) / 2
+    final['ticker'] = data['data']['symbol']
+    return final
 
-test()
+if __name__ == '__main__':
+    argv.pop(0)
+    full_output = []
+    for ticker in argv:
+       ret = download_data(ticker) 
+       out = process(ret)
+       full_output.append(out)
 
+    f = open("stocks.json","w")
+    json.dump(full_output,f,indent=4)
+    f.close()
